0ff238af1e58e56f13a0d41f03073073fb817b24
Dongwan Seoh <dongwanseoh@gmail.com>
2025-11-02 15:27:34 -0500

    adding fastapi base files

---
+++ Project/src/backend/app/routes/users.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, Depends, HTTPException, Request
+from pydantic import BaseModel
+from app.core.database import get_connection
+
+router = APIRouter(prefix="/user", tags=["user"])
+
+# ---------------------------
+# Models
+# ---------------------------
+
+class editUserInfoRequest(BaseModel):
+    name: str | None = None
+    phone: str | None = None
+    profile_data: list | None = None
+
+class editUserEmailRequest(BaseModel):
+    email: str
+
+class editUserPasswordRequest(BaseModel):
+    new_password: str
+
+# ---------------------------
+# Routes
+# -----------------
